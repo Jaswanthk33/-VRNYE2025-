1e2d3cc4a4fba04504a2d988cadce2e5c4c0e10a
Jaswanth Kandati <jaswanth.kandati@gmail.com>
2024-12-07 19:38:23 +0530

    remove navbar from app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import Features from "./components/Features"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
-import HowItWorks from "./components/HowItWorks"
-import Navbar from "./components/NavBar"
 import TicketCard from "./components/TicketCard"
 
 const App = () => {
 
   return (
     <>
-      {/* <Navbar /> */}
       <Hero />
       <Highlights />
       <Features />
